c8c918c26933a2614932de2d1f97629b991fe684
nwrox <herikz.nawarro@gmail.com>
2017-04-28 01:17:32 +0000

    Update convert-dec-bin.js
    Novo método para converter binário em decimal.

--- convert-dec-bin.js
+++ convert-dec-bin.js
@@ -1,13 +1,27 @@
+const binToDec = (b) => {
+	const arr = b.toString().split('')
+
+	let exp = arr.length - 1
+	  , sum = 0
+	 
+	for(let c of arr){	  
+	  if(c === '1' || c === 1) sum += Math.pow(2, exp)
+	  exp--
+	}
+		  
+  return sum
+}
+
 const decToBin = (n) => {
 	const log2 = Math.round(Math.log2(n));
 	let bin = ''
-		, pow;
+		, pow
 	
 	for(let i = log2; i >= 0; i--){
 		pow = Math.pow(2, i)
 		
 		if(pow <= n){
-			n -= pow;
+			n -= pow
 			bin += '1'
 		} else 
 			bin += '0'
@@ -16,4 +30,4 @@ const decToBin = (n) => {
 	return bin
 }
 
-module.exports = decToBin
+module.exports = { binToDec, decToBin }
