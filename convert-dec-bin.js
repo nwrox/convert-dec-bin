2084258f31d41aa48672ef63bf5e13f59d69d0c8
nwrox <herikz.nawarro@gmail.com>
2017-05-05 15:15:49 -0300

    Update convert-dec-bin.js
    Atualização usando apenas reduce.

--- convert-dec-bin.js
+++ convert-dec-bin.js
@@ -2,24 +2,19 @@ const binToDec = (b) => {
 	const arr = b.toString().split('')
 	let exp = arr.length - 1;
 
-	return arr.map((elem, index) => {
-		let dec = 0;
-
-		(elem === '1') ? dec = Math.pow(2, exp--) : (exp--, dec = 0)
-
-		return dec
-	}).reduce((acc, elem) => acc + elem, 0)
+	return arr.reduce(
+		(acc, curr) => (curr === '1') ? acc + Math.pow(2, exp--) : ( exp--, acc + 0 ), 0
+	)
 }
 
 const decToBin = (n) => {
-	return reverseSeq(getLog2(n)).map(elem => {
-		const pow = Math.pow(2, elem)
-		let bin = '';
-		
-		(pow <= n) ? (n -= pow, bin = '1') : bin = '0'
+	return reverseSeq(getLog2(n)).reduce(
+		(acc, curr) => {
+			const pow = Math.pow(2, curr)
 
-		return bin
-	}).join('')
+			return (pow <= n) ? ( n -= pow, acc + '1' ) : acc + '0'
+		}, ''
+	)
 }
 
 const getLog2 = (n) => Math.round(Math.log2(n))
